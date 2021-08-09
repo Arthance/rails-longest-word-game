e8bd4b16ed1af97710a159e9fcbc977fc0d7354c
Arthur Herminie <arthur.herminie@gmail.com>
2021-08-09 19:10:35 +0200

    update - TODO API PARSING

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  def new
+    @letters = (0...10).map { ('a'..'z').to_a[rand(26)].upcase }
+  end
+
+  def score
+    @word = params[:word]
+  end
+end
